38ad7c47b5809e0b28f8d33346b31c914f0b4296
Y7000 <kyle201817147@gmail.com>
2019-01-15 14:02:55 +0800

    Init a project
    Signed-off-by: Y7000 <kyle201817147@gmail.com>

---
+++ app/src/main/java/com/demo/gomoku/view/GomokuActivity.java
@@ -0,0 +1,15 @@
+package com.demo.gomoku.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.demo.gomoku.R;
+
+public class GomokuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
